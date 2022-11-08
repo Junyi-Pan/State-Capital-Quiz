0372a307679f44aec2510e1773ed1b66d2279ce0
timpan <timmypanmmy@gmail.com>
2022-11-08 12:20:09 -0500

    load csv data on start up

--- app/src/main/java/edu/uga/cs/statecapitalquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/statecapitalquiz/MainActivity.java
@@ -1,16 +1,72 @@
 package edu.uga.cs.statecapitalquiz;
 
+import static android.app.PendingIntent.getActivity;
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StatesData statesData = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            InputStream ins = getAssets().open("state_capitals.csv");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
+            String line;
+            reader.readLine();
+            try {
+                while ((line = reader.readLine()) != null) {
+                    String[] token = line.split(",");
+                    State state = new State();
+                    state.setStateName(token[0]);
+                    state.setCapital(token[1]);
+                    state.setCity1(token[2]);
+                    state.setCity2(token[3]);
+                    state.setStateHood(token[4]);
+                    state.setCapitalSince(token[5]);
+                    state.setCapitalRank(token[6]);
+                    new StateDBWriter().execute(state);
+                }
+            } catch (IOException e) {
+                System.out.print("something wrong");
+            }
+        } catch (IOException e) {
+            System.out.print("file doesn't exist");
+        }
     }
 
+    // This is an AsyncTask class (it extends AsyncTask) to perform DB writing of a job lead, asynchronously.
+    public class StateDBWriter extends AsyncTask<State, State> {
 
+        // This method will run as a background process to write into db.
+        // It will be automatically invoked by Android, when we call the execute method
+        // in the onClick listener of the Save button.
+        @Override
+        protected State doInBackground( State... states ) {
+            statesData.storeState(states[0]);
+            return states[0];
+        }
+        // This method will be automatically called by Android once the writing to the database
+        // in a background process has finished.  Note that doInBackground returns a JobLead object.
+        // That object will be passed as argument to onPostExecute.
+        // onPostExecute is like the notify method in an asynchronous method call discussed in class.
+        @Override
+        protected void onPostExecute( State state ) {
+            // Show a quick confirmation message
+            Log.d(TAG, "State saved: " + state );
+        }
+    }
 }
