3e7e6cdef91a1eaf35427906bfc8a3a338c2bc58
Sunny <88217547+SS2023@users.noreply.github.com>
2022-11-09 18:10:11 -0500

    new commit

---
+++ app/src/main/java/edu/uga/cs/statecapitalquiz/QuizCompleted.java
@@ -0,0 +1,14 @@
+package edu.uga.cs.statecapitalquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizCompleted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_completed);
+    }
+}
